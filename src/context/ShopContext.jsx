1954a007d318038519ee4fea2180dae28f6ad415
karnpun9 <chayada.pun9@gmail.com>
2024-12-02 15:50:10 +0700

    user url edit

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -11,7 +11,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "฿";
   const delivery_fee = 10;
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_USER_URL;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
